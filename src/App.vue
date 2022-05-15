e9d5eda4234739c95ee156e7a8b6d9e8b887bfe8
techdude101 <techdude1011@gmail.com>
2022-05-15 09:16:12 +0100

    Added deployment config

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
       </b-row>
       <b-row>
     <input-text
-      labelText="Title"
-      placeholderText="Bug Description"
+      labelText="Description"
+      placeholderText="Description"
       tooltipText=""
       inputElementName="bugDescriptionInput"
       labelElementName="bugDescriptionLabel"
@@ -66,8 +66,16 @@
     <b-row class="mt-4">
       <b-col>
         <div v-if="hasMounted">
-        <strong><label class="pt-2 pb-2 float-start"
-          for="outputTextArea" name="outputTextLabel">Output</label></strong>
+          <strong>
+            <label class="pt-2 pb-2 float-start editIcon"
+              for="outputTextArea" name="outputTextLabel">Output
+              <a href="editTemplate"><span id="editIcon" class="float-right">
+                <!-- Edit icon -->
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z"/></svg>
+                </span>
+              </a>
+            </label>
+          </strong>
         <b-form-textarea
           id="textarea"
           placeholder="Enter something..."
@@ -126,7 +134,7 @@ export default {
       stepTextValue: '',
       stepBuffer: [],
       steps: [],
-      titlePosition: 0,
+      descriptionPosition: 0,
       inputsDisabled: false,
     };
   },
@@ -143,7 +151,7 @@ export default {
       }
 
       if (this.description.length > 0) {
-        text = replaceTextAt(text, '<title>', this.description, this.titlePosition);
+        text = replaceTextAt(text, '<description>', this.description, this.descriptionPosition);
       }
       switch (this.ticketNumbersData.length) {
         case 1:
@@ -211,7 +219,7 @@ export default {
       }
     },
     handleOutputTextChange() {
-      this.titlePosition = this.getTitlePosition();
+      this.descriptionPosition = this.getTitlePosition();
       this.disableInputFields();
     },
     handleListChange(value) {
@@ -252,7 +260,7 @@ export default {
       // Clear the add step input field
       this.stepTextValue = '';
     },
-    getTitlePosition() {
+    getDescriptionPosition() {
       if (this.outputText == null) return 0;
       if (this.description === '') return 0;
       return this.outputText.search(this.description);
