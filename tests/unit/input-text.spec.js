97bb1408fbaa0216ff3dc30144aef282682f59ab
techdude101 <techdude1011@gmail.com>
2022-05-16 20:59:46 +0100

    Fixed unit tests for input-text after changing from BFormInput to input

--- tests/unit/input-text.spec.js
+++ tests/unit/input-text.spec.js
@@ -1,10 +1,6 @@
 import Vue from 'vue';
-import { BFormInput, BootstrapVue } from 'bootstrap-vue';
 import { shallowMount } from '@vue/test-utils';
 import InputText from '../../src/components/InputText.vue';
-import CustomToolTip from '../../src/components/CustomToolTip.vue';
-
-Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
@@ -15,7 +11,7 @@ test('Default values when no props passed', () => {
   expect(titleLabel.text()).toBe('Label');
   expect(titleLabel.attributes('name')).toBe('label-name');
 
-  const inputField = wrapper.findComponent(BFormInput);
+  const inputField = wrapper.find('input');
   expect(inputField.attributes('placeholder')).toBe('Placeholder');
   expect(inputField.attributes('name')).toBe('input-name');
 });
@@ -40,39 +36,7 @@ test('Label and placeholder are displayed', () => {
   expect(titleLabel.text()).toBe(title);
   expect(titleLabel.attributes('name')).toBe(labelName);
 
-  const inputField = wrapper.findComponent(BFormInput);
+  const inputField = wrapper.find('input');
   expect(inputField.attributes('placeholder')).toBe(placeholder);
   expect(inputField.attributes('name')).toBe(inputName);
-});
-
-test('Tooltip is displayed when input element is clicked', async () => {
-  const title = 'Title';
-  const placeholder = 'Title Placeholder';
-  const tooltip = 'Title Tooltip';
-  const inputName = 'title-input';
-  const labelName = 'title-label';
-  const wrapper = shallowMount(InputText, {
-    propsData: {
-      labelText: title,
-      labelElementName: labelName,
-      placeholderText: placeholder,
-      tooltipText: tooltip,
-      inputElementName: inputName,
-    },
-  });
-
-  const titleLabel = wrapper.find('label');
-  expect(titleLabel.text()).toBe(title);
-  expect(titleLabel.attributes('name')).toBe(labelName);
-
-  const inputField = wrapper.findComponent(BFormInput);
-  expect(inputField.attributes('placeholder')).toBe(placeholder);
-  expect(inputField.attributes('name')).toBe(inputName);
-
-  expect(wrapper.findComponent(CustomToolTip).exists()).toBe(false);
-  await inputField.trigger('click');
-  const tooltipField = wrapper.findComponent(CustomToolTip);
-  expect(tooltipField.exists()).toBe(true);
-  expect(tooltipField.attributes('tooltiptext')).toBe(tooltip);
-  expect(tooltipField.attributes('targetelementname')).toBe(inputName);
 });
