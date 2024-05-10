a0429714ac34d99b34c15709888faec70e35fc95
techdude101 <techdude1011@gmail.com>
2024-05-10 20:37:02 +0100

    Fix deployment issue?

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
 // vue.config.js
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/edit-template-page/' : '/',
   configureWebpack: (config) => {
     config.output.filename = '[name].js';
   },
